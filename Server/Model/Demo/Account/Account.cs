3c2ea704e37ddd4f6a957bf9ec1600001f5b043d
Tonydsnape <50350093+Tonydsnape@users.noreply.github.com>
2024-01-27 18:45:08 +0800

    登录系统01
    第一节课的代码

---
+++ Server/Model/Demo/Account/Account.cs
@@ -0,0 +1,20 @@
+﻿namespace ET.Account
+{
+    public enum AccountType
+    {
+        General = 0,    //普通账户
+        
+        BlackList = 1,  //黑名单账户
+    }
+    
+    public class Account: Entity, IAwake
+    {
+        public string AccountName;  //账户名
+        
+        public string Password;     //密码
+        
+        public long CreateTime;     //创建时间
+        
+        public int AccountType;     //账户类型
+    }
+}
